e408ad86604c80841fda001045217d6b3fe0226d
divyatasanadhya <68656968+divyatasanadhya@users.noreply.github.com>
2022-03-26 15:56:13 +0530

    Add files via upload

---
+++ reverse.js.js
@@ -0,0 +1,11 @@
+let reverse = 0;
+let number = 123456;
+let lastDigit;
+
+while(number != 0){
+	lastDigit = number % 10;
+  reverse = reverse * 10 + lastDigit;
+  number = Math.floor(number/10);
+}
+
+console.log("Reverse number : "+reverse);
